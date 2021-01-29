12150076f868c21367f861fff9b6df04a1243489
Melli Wara <etaphs@gmail.com>
2021-01-29 00:06:49 -0700

    Make plus button as tall as possible

--- extension/fg/icons.js
+++ extension/fg/icons.js
@@ -2,17 +2,17 @@ class AbIcons {
     
     setSpinner(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
-        ankiExport.style = "grid-template-rows: 24px auto 0 0;";
+        ankiExport.style = "grid-template-rows: calc(100% - 24px) auto 0 0;";
     }
 
     setSuccess(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
-        ankiExport.style = "grid-template-rows: 24px 0 auto 0;";
+        ankiExport.style = "grid-template-rows: calc(100% - 24px) 0 auto 0;";
     }
 
     setAlert(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
-        ankiExport.style = "grid-template-rows: 24px 0 0 auto;";
+        ankiExport.style = "grid-template-rows: calc(100% - 24px) 0 0 auto;";
     }
 
     disableAll() {
