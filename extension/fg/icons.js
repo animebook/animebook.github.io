62a93d0c9bbf2bdbee5f1770fd9a01104737d499
Soamsy <thsoams@gmail.com>
2022-06-08 13:22:57 -0600

    Don't animate the loading spinner for every caption in the sidebar
    Even though the loading spinner for when a card is being made
    is "invisible", it's still animating in the background, causing
    massive performance issues for large subtitle files. Hiding
    things with css grid was a mistake.
    
    For #45 and #53

--- extension/fg/icons.js
+++ extension/fg/icons.js
@@ -3,16 +3,19 @@ class AbIcons {
     setSpinner(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
         ankiExport.style = "grid-template-rows: calc(100% - 24px) auto 0 0;";
+        this.enableSpinnerAnimation(ankiExport);
     }
 
     setSuccess(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
         ankiExport.style = "grid-template-rows: calc(100% - 24px) 0 auto 0;";
+        this.disableSpinnerAnimation(ankiExport);
     }
 
     setAlert(captionId) {
         var ankiExport = document.getElementById("anki-export-" + captionId)
         ankiExport.style = "grid-template-rows: calc(100% - 24px) 0 0 auto;";
+        this.disableSpinnerAnimation(ankiExport);
     }
 
     disableAll() {
@@ -29,5 +32,12 @@ class AbIcons {
         var elements = document.getElementsByClassName('anki-export');
         Array.from(elements).forEach(el => el.style = '');
     }
-    
+
+    enableSpinnerAnimation(ankiExport) {
+        ankiExport.querySelector(".spinner-icon").style = "animation: rotate 1s infinite 0s steps(8)";
+    }
+
+    disableSpinnerAnimation(ankiExport) {
+        ankiExport.querySelector(".spinner-icon").style = ""
+    }
 }
